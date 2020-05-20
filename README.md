c3976439a78d275351921f84bf27521e634c0507
Matheus Cunha <matheuz.a@gmail.com>
2020-05-20 17:44:16 +0200

    feat(piechart): Changing from text to piechart
    Using Wibox.widget.piechart to display values based on the memory
    consumption (only RAM, swap is dropped).

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-# AwesomeWM Memory Notifier plug-in
----
+<h1 align="center">AwesomeWM Memory Notifier plug-in</h1>
 
 Simple and minimalistic Memory (RAM) notifier for AwesomeWM.
+Using [Wibox widget piechart](https://awesomewm.org/apidoc/widgets/wibox.widget.piechart.html) to display memory usage in GB and percentage.
 
 ![Screenshot](/screenshot.png?raw=true "Screenshot")
 
-RAM notifier plugin code initially was based on [deficient/calendar](https://github.com/deficient/calendar.git),
-later Lain came in place to render markup easily.
+RAM notifier plugin code was initially based on [deficient/calendar](https://github.com/deficient/calendar.git),
+displaying a small text using [Naughty](https://awesomewm.org/doc/api/libraries/naughty.html).
+
+Later it evolved into a pie chart notification using [streetturtle](https://github.com/streetturtle/awesome-wm-widgets/blob/master/ram-widget/ram-widget.lua) implementation as an inspiration.
 
 ### Installation
 
@@ -18,28 +20,43 @@ cd ~/.config/awesome/plugins
 git clone https://github.com/macunha1/awesomewm-memory-notifier ~/.config/awesome/plugins/memory-notifier
 ```
 
+If your AwesomeWM config is a git repository, you can add as a submodule with
+
+``` bash
+cd ~/.config/awesome
+
+git submodule add -b master \
+    -f --name memory-plugin \
+    https://github.com/macunha1/awesomewm-memory-notifier \
+    plugins/memory-notifier
+
+git submodule sync --recursive .
+```
+
 ### Usage
 
 And then import the plugin into your `rc.lua`:
 
 ```lua
 -- load the widget code
-local memory_wid = require("plugins.memory-notifier")
+local memory_widget = require("plugins.memory-notifier")
 
 -- attach it as popup to your memory widget:
-memory_wid({
-    fg_color  = theme.fg_focus,  -- You can customize it with your theme's colors
-    fg_normal = theme.fg_normal, -- Just like these, quite simple
-}):attach(some_memory_widget)
-```
+mem_widget({
+    colors = {
+        theme.fg_focus,
+        theme.bg_normal,
+        theme.fg_normal,
+    },
+    font  = theme.font, -- not supported
+    fg    = theme.fg_normal,
+    bg    = theme.bg_focus,
 
+    border_width = theme.border_width,
+    border_color = theme.border_color
+}):attach(ram_wid)
+```
 
 ### Requirements
 
 * [awesome 4.0](http://awesome.naquadah.org/). May work on 3.5 with minor changes.
-
-## Authors
-
-* [**Matheus Cunha** ](https://github.com/macunha1)
-
-See also the list of [contributors](https://github.com/macunha1/awesomewm-memory-notifier/contributors) who participated in this project.<Paste>
--- memory.lua
+++ memory.lua
@@ -1,137 +1,156 @@
 #!/usr/bin/env lua
 
-local os = os
-local capi = {
-    mouse = mouse,
-    screen = screen
-}
-local awful   = require("awful")
-local lain    = require("lain")
-local naughty = require("naughty")
+local awful     = require("awful")
+local dpi       = require("beautiful.xresources").apply_dpi
+local beautiful = require("beautiful")
+local naughty   = require("naughty")
+local watch     = require("awful.widget.watch")
+local wibox     = require("wibox")
 
 local Memory = {}
 Memory.__index = Memory
 
 function Memory:new(args)
-    return setmetatable({}, {__index = self}):init(args)
+	return setmetatable({}, {__index = self}):init(args)
 end
 
 function Memory:init(args)
-    self.fg_normal  = args.fg_normal or "#ffffff"
-    self.fg_color   = args.fg_color or "#00ff00"
-    self.font       = args.font or "monospace"
+	self.colors = args.colors or {
+		beautiful.fg_focus,
+		beautiful.fg_normal,
+		beautiful.bg_focus,
+	}
 
-    return self
+	self.border_width = args.border_width or beautiful.border_width or 0
+	self.border_color = args.border_color or beautiful.border_color or 0
+	self.font = args.font or beautiful.font
+
+	-- change the widget size, pie chart is resized by the height
+	local widget_height = args.height or 150
+	
+	self.widget = wibox {
+		height  = dpi(widget_height),
+		width   = dpi(widget_height * 3),
+		ontop   = true,
+		expand  = true,
+		
+		fg = args.fg or beautiful.fg_normal,
+		bg = args.bg or beautiful.bg_normal,
+		
+		border_width    = self.border_width,
+		border_color    = self.border_color,
+		max_widget_size = 500
+	}
+
+	self.widget:setup {
+		border_width = self.border_width,
+		colors       = self.colors,
+		font         = self.font,
+
+		display_labels = true,
+		id             = 'pie',
+		widget         = wibox.widget.piechart
+	}
+
+	self.visible = false
+
+	return self
 end
 
-function Memory:fmt(free_stdout)
-    local mem_table = {}
+function Memory:format(free_stdout)
+	local mem_table = {}
 
-    -- > free # stdout example
-    --               total        used        free      shared  buff/cache   available
-    -- Mem:        1006536       69664      845824         428       91048      819760
-    -- Swap:       2013180           0     2013180
+	-- > free # stdout example, some whitespaces omitted
+	--        total    used   free     shared  buff/cache  available
+	-- Mem:   1006536  69664  845824   428     91048       819760
+	-- Swap:  2013180  0      2013180
 
-    mem_table["\nTotal"],
-        mem_table["Used"],
-        mem_table["Free"],
-        mem_table["Shared"],
-        mem_table["Buff"], -- buff/cache
-        mem_table["Available"],
-        mem_table["Swap Total"],
-        mem_table["Swap Used"],
-        mem_table["Swap Free"] =
-        free_stdout:match("(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*Swap:%s*(%d+)%s*(%d+)%s*(%d+)")
+	mem_table["Total"],
+		mem_table["Used"],
+		mem_table["Free"],
+		mem_table["Shared"],
+		mem_table["Buff"], -- buff/cache
+		mem_table["Available"],
+		mem_table["Swap Total"],
+		mem_table["Swap Used"],
+		mem_table["Swap Free"] =
+			free_stdout:match("(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*Swap:%s*(%d+)%s*(%d+)%s*(%d+)")
 
-    local display_order = {
-        "Used",
-        "Buff",
-        "Free",
-        "\nTotal",
-    }
-    local t = {}
+	local format = function(t, key)
+		local value = t[key]
+		local str = string.format(
+			"%s: %3.2f%% (%3.1f GB)",
+			key,
+			(value / t["Total"]) * 100,
+			(value / 1048576)
+		)
 
-    for _, key in ipairs(display_order) do
-        table.insert(
-            t,
-            lain.util.markup.fontfg(
-                self.font,
-                self.fg_normal,
-                lain.util.markup.bold(key .. ":\t")
-            )
-        )
+		return { str, value }
+	end
 
-        -- 1 GB = 1048576 = 1024 * 1024 (since free prints in kB)
-        table.insert(
-            t,
-            lain.util.markup.fontfg(
-                self.font,
-                self.fg_color,
-                string.format(
-                    "%.3f\t<b>GB</b>",
-                    mem_table[key] / 1048576
-                )
-            ) .. "\n"
-        )
-    end
+	local display_fields = {
+		"Used",
+		"Free",
+		"Shared",
+		"Buff"
+	}
+	
+	local t = {}
 
-    return table.concat(t)
+	for _, key in ipairs(display_fields) do
+		table.insert(t, format(mem_table, key))
+	end
+	
+	return t
 end
 
 function Memory:show()
-    awful.spawn.easy_async(
-        [[ bash -c "free | grep -z Mem.*Swap.*" ]],
-        function(stdout, stderr, reason, exit_code)
-            local title = "Memory status"
-            local mem_text = lain.util.markup.font(
-                self.font,
-                "\n" .. self:fmt(stdout)
-            )
+	self.visible = true
 
-            if naughty.replace_text and self.notification then
-                naughty.replace_text(
-                    self.notification,
-                    title,
-                    mem_text
-                )
-            else
-                self:hide()
-                self.notification = naughty.notify({
-                    title = title,
-                    text = mem_text,
-                    timeout = 2,
-                    hover_timeout = 0.5,
-                    screen = capi.mouse.screen
-                })
-            end
-        end
-    )
+	-- placement goes here to "follow" the focused screen
+	awful.placement.top_right(
+		self.widget,
+		{
+			margins = {top = 25, right = 10},
+			parent = awful.screen.focused()
+		}
+	)
+
+	awful.spawn.easy_async(
+		[[ bash -c "free | grep -z Mem.*Swap.*" ]],
+		function(stdout, stderr, reason, exit_code)
+			self.widget.pie.data_list = self:format(stdout)
+			-- avoids race conditions
+			self.widget.visible = self.visible
+		end
+	)
 end
 
 function Memory:hide()
-    if self.notification then
-        naughty.destroy(self.notification)
-        self.notification = nil
-    end
+	self.visible = false
+	self.widget.visible = false
+end
+
+function Memory:toggle()
+	self.widget.visible = not self.widget.visible
 end
 
 function Memory:attach(widget)
-    local show = function()
-        self:show()
-    end
+	local show = function()
+		self:show()
+	end
 
-    local hide = function()
-        self:hide()
-    end
+	local hide = function()
+		self:hide()
+	end
 
-    widget:connect_signal("mouse::enter", show)
-    widget:connect_signal("mouse::leave", hide)
-    widget:buttons(awful.util.table.join(awful.button({}, 1, show)))
+	local toggle = function()
+		self:toggle()
+	end
+
+	widget:connect_signal("mouse::enter", show)
+	widget:connect_signal("mouse::leave", hide)
+	widget:buttons(awful.util.table.join(awful.button({}, 1, toggle)))
 end
 
-return setmetatable(
-    Memory,
-    {
-        __call = Memory.new
-    }
-)
+return setmetatable(Memory, { __call = Memory.new })
